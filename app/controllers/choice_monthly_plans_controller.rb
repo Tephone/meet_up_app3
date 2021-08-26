1d9d65281eefb1413fd5fad89d88d87562b643f1
naoko kasahara <kassa0918@gmail.com>
2021-08-26 15:50:03 +0900

    定期購入メゾット定義

--- app/controllers/choice_monthly_plans_controller.rb
+++ app/controllers/choice_monthly_plans_controller.rb
@@ -8,6 +8,7 @@ class ChoiceMonthlyPlansController < ApplicationController
   def create
     @choice_monthly_plan = current_student.build_choice_monthly_plan(choice_monthly_plan_params)
     if @choice_monthly_plan.save
+      current_student.purchase_tickets.create!(ticket_id: @choice_monthly_plan.monthly_plan.ticket.id, deadline: Date.current.end_of_month)
       redirect_to student_path(current_student), notice: 'プランを選択しました'
     else
       render :new
---
+++ lib/tasks/monthly_task.rake
@@ -0,0 +1,8 @@
+namespace :monthly_task do
+  desc '定期プランでチケットを購入する処理'
+  task ticket_subscription: :environment do
+    if Date.current == Date.current.beginning_of_month
+      ChoiceMonthlyPlan.ticket_subscription
+    end
+  end
+end
