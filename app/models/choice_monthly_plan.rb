c284e8e28fa8dd4ab5bd006e12f6d9c5a0f0cad7
naoko kasahara <kassa0918@gmail.com>
2021-08-26 15:51:33 +0900

    sgcop先生

--- app/models/choice_monthly_plan.rb
+++ app/models/choice_monthly_plan.rb
@@ -3,7 +3,7 @@ class ChoiceMonthlyPlan < ApplicationRecord
   belongs_to :student
 
   def self.ticket_subscription
-    self.all.each do |plan|
+    self.all.find_each do |plan|
       plan.student.purchase_tickets.create(ticket_id: plan.monthly_plan.ticket.id, deadline: Date.current.end_of_month)
     end
   end
